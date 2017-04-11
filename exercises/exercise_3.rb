e4df2db2d619982229b3ceb3897296062d34074c
sadooghi <gh.sadooghi@gmail.com>
2017-04-11 23:36:14 +0000

    ex3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -5,4 +5,6 @@ require_relative './exercise_2'
 puts "Exercise 3"
 puts "----------"
 
-# Your code goes here ...
+@store3 = Store.find(3)
+@store3.destroy
+puts "there are #{Store.count} stores in DB"
