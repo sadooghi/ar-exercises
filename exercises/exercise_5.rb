933ee4378c9f94451f3811b6e1c4600444a9ebce
sadooghi <gh.sadooghi@gmail.com>
2017-04-12 00:20:42 +0000

    ex5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,4 +8,8 @@ puts "Exercise 5"
 puts "----------"
 
 @store_annual_revenue = Store.sum(:annual_revenue)
+puts "total annual revenue is #{@store_annual_revenue}"
 @average_annual_revenue = @store_annual_revenue / Store.count
+puts "average annual revenue is #{@average_annual_revenue}"
+@average_annual_revenue_more_than_1M_rev = Store.where("annual_revenue >?", 1000000).sum(:annual_revenue)/Store.where("annual_revenue >?", 1000000).count
+puts "average annual revenue for stores with annual revenue of more than 1 M is #{@average_annual_revenue_more_than_1M_rev}"
